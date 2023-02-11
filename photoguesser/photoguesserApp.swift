1ad86608a8b0d6b04458816876ad40b5c703544a
Adrian Sergheev <sergheevdev@icloud.com>
2023-02-11 19:02:12 +0100

    Initial Commit

---
+++ photoguesser/photoguesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoguesserApp.swift
+//  photoguesser
+//
+//  Created by Adrian Sergheev on 2023-02-11.
+//
+
+import SwiftUI
+
+@main
+struct photoguesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
