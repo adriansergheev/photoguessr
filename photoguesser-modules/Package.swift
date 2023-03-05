835fe6878066f12e60c1640d25e7e5078e09c5f2
Adrian Sergheev <sergheevdev@icloud.com>
2023-03-05 12:54:42 +0100

    nuke 12

--- photoguesser-modules/Package.swift
+++ photoguesser-modules/Package.swift
@@ -26,7 +26,7 @@ let package = Package(
 		.package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.50.2"),
 		.package(url: "https://github.com/pointfreeco/swift-dependencies", from: "0.1.4"),
 		.package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.8.2"),
-		.package(url: "https://github.com/kean/Nuke", branch: "nuke-12"),
+		.package(url: "https://github.com/kean/Nuke", from: "12.0.0"),
 		.package(url: "https://github.com/spacenation/swiftui-sliders", .upToNextMajor(from: "2.1.0")),
 		.package(url: "https://github.com/pointfreeco/swift-nonempty", branch: "main")
 	],
